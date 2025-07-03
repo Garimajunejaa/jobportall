a8e366b1cc4684322013fbcab52eaa9214769a93
Garimajunejaa <junejag51@gmail.com>
2025-07-03 16:51:51 +0530

    M

--- frontend/src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = "https://jobportall-1-gylx.onrender.com";
-
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
