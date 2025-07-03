f48aacf53912454dc2b4ed3aa0cb43ff55622625
Garimajunejaa <junejag51@gmail.com>
2025-07-03 17:34:17 +0530

    Update backend CORS configuration to use environment variable

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,16 @@ import jobRecommendationRouter from "./routes/jobRecommendation.route.js";
 
 const app = express();
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 app.use(cors({
     origin: function(origin, callback) {
-        const allowedOrigins = ['https://jobportall-1-gylx.onrender.com', 'http://localhost:3000'];
+        const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS ? process.env.CORS_ALLOWED_ORIGINS.split(',') : [];
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, origin);
         } else {
-            callback(null, false);
+            callback(new Error('Not allowed by CORS'));
         }
     },
     credentials: true
